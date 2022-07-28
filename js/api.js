8d0e6957dd4221fa7b9da531dc84ce68117da30e
ana sanchez <sanchezcabelloanacristina@gmail.com>
2022-07-27 21:07:14 -0500

    arreglos css

--- js/api.js
+++ js/api.js
@@ -5,21 +5,30 @@ let genero_seleccionado = null;
 //encontrar elementos de html
 
 const el_lista = document.getElementById("generos-musicales");
-const el_imagen = document.getElementById("imagen-del-genero")
+const el_imagen = document.getElementById("imagen")
 const el_titulo = document.querySelector("div#contenido h2")
-const el_informacion = document.querySElectos("div#contenido h3")
+const el_canciones = document.querySelector("div#contenido h2")
 
 fetch(uri + "datos.json").then(function(response) {
    return response.json();
 }).then(function(datos) {
     for(let i = 0; i < datos.length; i+=1) {
+      console.log(datos[i]);
         let nuevo_boton = document.createElement("button");
         nuevo_boton.innerHTML += datos[i].nombre;
         el_lista.appendChild(nuevo_boton);
         nuevo_boton.addEventListener("click", function () {
-          el_imagen.src = URL + datos[i].imagen;
+          el_imagen.src = uri + datos[i].imagen;
           el_titulo.innerHTML = datos[i].nombre;
-          el_informacion.innerHTML = datos[i].informacion;
+        
+          if(datos[i].canciones !=undefined) {
+            el_titulo.innerHTML +="\n<h3>Canciones de este genero:</h3><ul>";
+            for(let j = 0; j < datos[i].canciones.length; j+=1) {
+              el_titulo.innerHTML += "<li>" + datos[i].canciones[j] + "</li>";
+            }
+            el_titulo.innerHTML  += "</ul>"
+          }
+
           if(genero_seleccionado != null){
             genero_seleccionado.classList.remove("seleccionado")
           }
